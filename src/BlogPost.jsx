07f465af45e63eefa1fe08411e369a1417910289
sara segura <sarasegura224@gmail.com>
2023-11-09 11:05:19 -0500

    commit

--- src/BlogPost.jsx
+++ src/BlogPost.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./blogdata";
+import { useAuth } from "./auth";
 
 function BlogPost() {
   const navigate = useNavigate();
   const { slug } = useParams();
 
+  const auth = useAuth();
+
   const blogpost = blogdata.find((post) => post.slug === slug);
+
+  const canDelete =
+    auth.user?.isAdmin || blogpost.author === auth.user?.username;
 
   const returnToBlog = () => {
     navigate("/blog");
@@ -18,6 +24,8 @@ function BlogPost() {
       <button onClick={returnToBlog}>Volver al blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
+
+      {canDelete && <button>Eliminar blogpost</button>}
     </>
   );
 }
